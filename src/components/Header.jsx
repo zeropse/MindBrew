fe4eed3348fc5fb461289837c4284306e68145da
zeropse <129523087+zeropse@users.noreply.github.com>
2025-03-03 23:07:37 +0530

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,20 +8,20 @@ const Header = () => {
         className="p-4 cursor-pointer hover:text-gray-300"
         onClick={() => navigate("/")}
       >
-        <h1 className="text-4xl font-bold">motivateCore</h1>
-        <p className="text-sm pl-5 pt-1">motivate Your Life</p>
+        <h1 className="text-4xl font-bold">motivatecore</h1>
+        <p className="text-sm pl-5 pt-1">motivate your life</p>
       </div>
 
       <div className="p-4">
         <ul className="flex gap-4">
           <li
-            className="cursor-pointer hover:text-gray-300"
+            className="cursor-pointer hover:text-gray-300 p-3"
             onClick={() => navigate("/")}
           >
             Home
           </li>
           <li
-            className="cursor-pointer hover:text-gray-300"
+            className="cursor-pointer font-bold bg-white text-black p-3 rounded-md hover:bg-gray-300"
             onClick={() => navigate("/started")}
           >
             Get Started
