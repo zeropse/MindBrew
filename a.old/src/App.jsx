6317274c5daf174af469989be49bfc9dd6c8aefd
zeropse <129523087+zeropse@users.noreply.github.com>
2026-01-08 01:26:31 +0530

    init

---
+++ a.old/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RootLayout from "@/layout/RootLayout";
+import Home from "@/pages/Home";
+import QuotePage from "@/pages/QuotePage";
+import NotFound from "@/pages/NotFound";
+import Inspirations from "@/pages/Inspirations";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/inspiration" element={<Inspirations />} />
+          <Route path="/inspiration/:slug" element={<QuotePage />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
